cde210763cb792173d5f892dc9a00bb04132d084
Derek Honrubia <111546379+Dekyut@users.noreply.github.com>
2023-04-13 11:47:52 +0800

    Create book.java

---
+++ WARMUP/book.java
@@ -0,0 +1,8 @@
+package Honrubia;
+
+public class Book {
+	String[] name = {"System Analysis and Design","Android Application","Programming Concepts and Logic Formulation"};
+	String[] author = {"Gary B. Shelly","Corine Hosington","Rosauro E. Manuel"};
+	int[] copies = {2,3,4};
+
+}
