9b51939273ed43f708d1520c1189453339cafedb
Derek Honrubia <111546379+Dekyut@users.noreply.github.com>
2023-04-13 11:54:21 +0800

    Create WarmUpHonrubia.java

---
+++ WARMUP/WarmUpHonrubia.java
@@ -0,0 +1,55 @@
+package Honrubia;
+
+import java.util.Scanner;
+
+
+
+public class WarnUpHonrubia {
+	public static void main(String[] args) {
+		Book book = new Book();
+		System.out.println("-----------------------------");
+		System.out.println("ELECTRONIC BOOK RENTAL SYSTEM");
+		System.out.println("-----------------------------");
+
+		for (int x = 0; x < book.name.length; x++) {
+			System.out.print(x);
+			System.out.print(" - " + book.name[x] + " - " + book.author[x]);
+			System.out.println();
+		}
+
+		Scanner in = new Scanner(System.in);
+		boolean end = false;
+
+		while (end == false) {
+			System.out.println("---------------------------");
+			System.out.print("CHOOSE A NUMBER TO RENT YOUR FAVORITE BOOK: ");
+			int choice = in.nextInt();
+			String s = in.nextLine();
+			try {
+				if (book.copies[choice] > 0) {
+					System.out.println("YOU RENTED: " + book.name[choice] + " by "+ book.author[choice]);
+					book.copies[choice] -= 1;
+				}
+				else {
+					System.out.println("THERE ARE NO MORE COPIES IN STOCK");
+				}
+
+				System.out.println("RENT AGAIN? Y/N");
+				String choiceYN = in.nextLine();
+				if (choiceYN.equals("Y")||choiceYN.equals("y")) {
+					end = false;
+				} else if (choiceYN.equals("N")||choiceYN.equals("n")) {
+					end = true;
+				}
+			} catch (Exception e) {
+				System.out.println("INVALID INPUT");
+			}
+		}
+		System.out.println("EXITING SYSTEM");
+	}
+
+}
+
+
+
+
