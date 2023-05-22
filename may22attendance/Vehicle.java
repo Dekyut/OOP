6f504b3415ada985697d4aef5e3508f3d7915052
Derek Honrubia <111546379+Dekyut@users.noreply.github.com>
2023-05-22 09:38:32 +0800

    Create Vehicle.java

---
+++ may22attendance/Vehicle.java
@@ -0,0 +1,14 @@
+package may22attendance;
+
+public class Vehicle {
+	
+	int Speed = 28;
+	String Color = "white";
+	String Price = "$2,495,000";
+	
+	void Stop(){
+		System.out.println("The Boat has stopped");
+	}
+	
+
+}
