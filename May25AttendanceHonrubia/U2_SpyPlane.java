ff661b7d7d0f9bf5317929d7df8b3bd5813d461b
Derek Honrubia <111546379+Dekyut@users.noreply.github.com>
2023-05-25 10:09:19 +0800

    Update U2_SpyPlane.java

--- May25AttendanceHonrubia/U2_SpyPlane.java
+++ May25AttendanceHonrubia/U2_SpyPlane.java
@@ -1,5 +1,8 @@
 package may22attendance;
 
+//This class extends from the Vehicle class in order for the method Stop() to work and print
+//whatever the method Stop() in the Vehicle class have inside.
+
 public class U2_SpyPlane extends Vehicle {
 	
 	 int WingSpan = 103;
@@ -8,6 +11,7 @@ public class U2_SpyPlane extends Vehicle {
 			System.out.println("The Plane is flying");
 		}
 		
+		//Here we Override the Stop() method in order for it to print the said method from the Vehicle Class
 		@Override
 		void Stop() {
 			super.Stop();
