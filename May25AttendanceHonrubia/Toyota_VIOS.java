5b921aa9e57efc9b8c9753105fd526b34713a9d2
Derek Honrubia <111546379+Dekyut@users.noreply.github.com>
2023-05-25 10:09:04 +0800

    Update Toyota_VIOS.java

--- May25AttendanceHonrubia/Toyota_VIOS.java
+++ May25AttendanceHonrubia/Toyota_VIOS.java
@@ -1,5 +1,8 @@
 package may22attendance;
 
+//This class extends from the Vehicle class in order for the method Stop() to work and print
+//whatever the method Stop() in the Vehicle class have inside.
+
 public class Toyota_VIOS extends Vehicle {
 	
     String TireType = "All Season Tire";
@@ -8,6 +11,7 @@ public class Toyota_VIOS extends Vehicle {
 		System.out.println("The Car is accelerating");
 	}
 	
+	//Here we Override the Stop() method in order for it to print the said method from the Vehicle Class
 	@Override
 	void Stop() {
 		super.Stop();
